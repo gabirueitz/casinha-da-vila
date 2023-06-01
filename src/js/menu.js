c401af5e1df67c37fcc9d67c628c1fb507fb85ff
Roger Gabiru <roger.gabiru@gmail.com>
2023-05-31 21:50:04 -0300

    adicionado responsividade

---
+++ src/js/menu.js
@@ -0,0 +1,8 @@
+function activeMenu() {
+    var x = document.getElementById("nav");
+    if (x.className === "nav") {
+      x.className += " responsive";
+    } else {
+      x.className = "nav";
+    }
+  }
