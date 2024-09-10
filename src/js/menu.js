e491d245f92169731628f234085c37d03e74b3e6
Roger Gabiru <roger.gabiru@gmail.com>
2024-09-10 18:46:03 -0300

    Novas fotos

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,8 +1,8 @@
 function ativaMenu() {
-    var x = document.getElementById("nav");
-    if (x.className === "nav") {
-        x.className += " responsive";
-    } else {
-        x.className = "nav";
-    }
+	var x = document.getElementById("nav");
+	if (x.className === "nav") {
+		x.className += " responsive";
+	} else {
+		x.className = "nav";
+	}
 }
